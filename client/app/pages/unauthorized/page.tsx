c8f44b4d97082011c6e52a70ce4bf9e515a58dd7
Dre <92935512+JPAVictoria@users.noreply.github.com>
2025-05-06 14:21:41 +0800

    update-commit

--- client/app/pages/unauthorized/page.tsx
+++ client/app/pages/unauthorized/page.tsx
@@ -1,19 +1,18 @@
 "use client";
 
-// import { useRouter } from "next/navigation";
-// import Cookies from "js-cookie";
+import { useRouter } from "next/navigation";
+import Cookies from "js-cookie";
 import { cn } from "@/lib/utils";
 import { DotPattern } from "@/components/magicui/dot-pattern";
-// import { Button } from "@mui/material";
 
 export default function Unauthorized() {
-  // const router = useRouter();
+  const router = useRouter();
 
-  // const handleLoginRedirect = () => {
-  //   Cookies.remove("token");
-  //   localStorage.removeItem("user");
-  //   router.push("/pages/login");
-  // };
+  const handleLoginRedirect = () => {
+    Cookies.remove("token");
+    localStorage.removeItem("user");
+    router.push("/pages/login");
+  };
 
   return (
     <div className="relative min-h-screen flex items-center justify-center overflow-hidden bg-background">
@@ -31,10 +30,17 @@ export default function Unauthorized() {
         <h1 className="font-bold text-4xl leading-[1.5] text-transparent bg-clip-text bg-gradient-to-r from-[#2F27CE] via-[#8681E7] to-[#8681E7]">
           Unauthorized
         </h1>
-        <p className="text-[#2d2d2d] tracking-wide text-md font-medium opacity-85">
-          You are not authorized to access this page. <br/> Please contact your
+        <p className="text-[#2d2d2d] tracking-wide text-md font-normal">
+          You are not authorized to access this page. <br /> Please contact your
           administrator for access.
         </p>
+
+        <button
+          onClick={handleLoginRedirect}
+          className="mt-2 text-sm px-4 py-1.5 border-2 border-[#2d2d2d59] rounded-md text-[#2d2d2d] font-medium cursor-pointer"
+        >
+          Back to login
+        </button>
       </div>
     </div>
   );
