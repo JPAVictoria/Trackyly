3a423f66e45c10191f61f3ddd859f06de26e2569
Dre <92935512+JPAVictoria@users.noreply.github.com>
2025-06-02 16:07:14 +0800

    fix-home

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,3 +1,12 @@
 export default function Home() {
-  return <><a href="https://trackyly.vercel.app/pages/login">Go to login</a></>
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <a
+        href="https://trackyly.vercel.app/pages/login"
+        className="text-4xl font-bold text-blue-600 transition-transform duration-300 ease-in-out transform hover:scale-110 hover:text-amber-500"
+      >
+        Go to login
+      </a>
+    </div>
+  );
 }
