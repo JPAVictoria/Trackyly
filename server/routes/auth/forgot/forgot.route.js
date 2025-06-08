9b9f0684f5a1be7af962d70252fb4148cbac4d7e
Dre <92935512+JPAVictoria@users.noreply.github.com>
2025-06-08 20:04:27 +0800

    fix bug

--- server/routes/auth/forgot/forgot.route.js
+++ server/routes/auth/forgot/forgot.route.js
@@ -21,7 +21,7 @@ router.post("/forgot", async (req, res) => {
       { expiresIn: "15m" }
     );
 
-    const resetUrl = `http://localhost:3000/pages/reset-password?token=${token}`; 
+    const resetUrl = `https://trackyly.onrender.com/pages/reset-password?token=${token}`; 
 
     const transporter = nodemailer.createTransport({
       service: "gmail",
