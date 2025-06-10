ed28ea1e64211984a9440be954a6147487d7cbba
Dre <92935512+JPAVictoria@users.noreply.github.com>
2025-06-10 11:01:24 +0800

    refactor using use state instead of store for stats

--- client/components/frontend/StatsOverViewBlock.tsx
+++ client/components/frontend/StatsOverViewBlock.tsx
@@ -1,24 +1,16 @@
 "use client";
-
-import { useEffect } from "react";
-import { Button } from "@mui/material";
+import { useEffect, useState } from "react";
+import { Button, Skeleton } from "@mui/material";
 import axios from "axios";
-import { useStatisticStore } from "@/app/stores/useStatisticStore";
 import Link from "next/link";
-import { buttonStyles} from "@/app/styles/styles"; 
+import { buttonStyles } from "@/app/styles/styles";
 import { apiUrl } from "@/app/utils/apiUrl";
 
 export default function StatsOverViewBlock() {
-  const {
-    sosCount,
-    merchCount,
-    loading,
-    error,
-    setSosCount,
-    setMerchCount,
-    setLoading,
-    setError,
-  } = useStatisticStore();
+  const [sosCount, setSosCount] = useState(0);
+  const [merchCount, setMerchCount] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     async function fetchStatistics() {
@@ -26,12 +18,9 @@ export default function StatsOverViewBlock() {
       setError(null);
 
       try {
-        const response = await axios.get(apiUrl(
-          "/user/statistics"),
-          {
-            withCredentials: true,
-          }
-        );
+        const response = await axios.get(apiUrl("/user/statistics"), {
+          withCredentials: true,
+        });
 
         setSosCount(response.data.sosCount);
         setMerchCount(response.data.merchCount);
@@ -44,13 +33,24 @@ export default function StatsOverViewBlock() {
     }
 
     fetchStatistics();
-  }, [setSosCount, setMerchCount, setLoading, setError]);
-
+  }, []); 
 
   if (loading) {
     return (
-      <div className="bg-white shadow-md rounded-sm h-64 p-4 w-full flex items-center justify-center">
-        <p>Loading...</p>
+      <div className="bg-white shadow-md rounded-sm h-64 p-4 w-full flex items-center">
+        <div className="w-1/2 flex flex-col items-center justify-center text-center">
+          <Skeleton variant="text" width={120} height={40} sx={{ mb: 2 }} />
+          <Skeleton variant="text" width={60} height={32} sx={{ mb: 2 }} />
+          <Skeleton variant="rounded" width={100} height={36} />
+        </div>
+
+        <div className="w-px h-3/4 bg-gray-500/50 mx-4" />
+
+        <div className="w-1/2 flex flex-col items-center justify-center text-center">
+          <Skeleton variant="text" width={140} height={40} sx={{ mb: 2 }} />
+          <Skeleton variant="text" width={60} height={32} sx={{ mb: 2 }} />
+          <Skeleton variant="rounded" width={100} height={36} />
+        </div>
       </div>
     );
   }
